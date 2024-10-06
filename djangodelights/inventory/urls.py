b19b783882c35e054a060fc911b8d87a23580af6
Angelina <angelinachigrinets@gmail.com>
2024-10-06 20:58:53 +0500

    Make inventory view

--- djangodelights/inventory/urls.py
+++ djangodelights/inventory/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
 
+urlpatterns = [path('inventory/', views.InventoryList.as_view(), name='inventory')]
+'''
 urlpatterns = [
     path('', views.home, name='home'),
-    path('inventory/', views.inventory_view, name='inventory'),
+    path('inventory/', views.InventoryList.as_view(), name='inventory'),
     path('menu/', views.menu_view, name='menu'),
     path('purchases/', views.purchases_view, name='purchases'),
     path('profit/', views.purchases_view, name='profit'),
     path('login/', views.login_view, name='login'),
 ]
+
+'''
--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Ingredient, Purchase, RecipeRequirement
 
-# Create your views here.
+
+#Ingredient view
+class InventoryList(ListView):
+    model = Ingredient
+    template_name = 'inventory/inventory.html' 
+    context_object_name = 'ingredients'
+
+
+
+
+#TODO class PurchaseCreate(CreateView):
+'''
+TODO record a new purchase, calculating the purchase total
+TODO subtract ingredients from Ingredient
+TODO update revenue, costs and profit
+TODO redirect to the purchase list
+'''
+
